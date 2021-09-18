d39f098b541de796a4733f2ee8c7ece463d00386
ErikBuer <buer94@gmail.com>
2021-09-18 13:39:34 +0200

    Added sin function.

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,16 @@
 extern crate std;
 
 use fixed::traits::Fixed;
+use fixed::*;
 
 /// Rase fixed number to an integer-valued power.
-/// - `base^power`.
+/// `base^power`.
+/// 
+/// /// ## Argument
+/// 
+/// * `base`  - The base number.
+/// * `power` - The power to raise 'base' to.
+/// 
 pub fn powi<T>( base:T, power:usize ) -> T
     where T: fixed::traits::Fixed
 {
@@ -17,6 +24,59 @@ pub fn powi<T>( base:T, power:usize ) -> T
         temp = temp*base;
     }
     return temp;
+}
+
+/// Take the elemtent-wise sine using a Taylor approximation of sin(x).
+/// 
+/// Sin is calculated using the following polynomial:
+/// 
+/// `sin(x) = x -( x^3/6.0 )+( x^5/120.0 )-( x^7/5040.0 )+( x^9/362880.0 )`
+/// 
+/// ## Argument
+/// 
+/// * `x` - The value to apply the operation to.
+/// 
+/// x must be wrapped to the -π=<x<π range.
+/// 
+/// ## Example
+/// 
+/// ```
+/// use fixed_trigonometry::*;
+/// use fixed::{types::extra::U18, FixedI32};
+/// 
+/// let mut x = FixedI32::<U18>::from_num(0);
+/// let mut y = sin(x);
+/// assert_eq!{ y.to_num::<f32>(), 0.0 };
+/// 
+/// x = FixedI32::<U18>::from_num(3.14/2.0);
+/// y = sin(x);
+/// assert_eq!{ y.to_num::<f32>(), 1.0000038 };
+/// ``` 
+pub fn sin<T>( x: T ) -> T
+    where T: fixed::traits::FixedSigned
+{
+    let pi_half:T = <T>::from_num(fixed::consts::PI/2);
+
+    let mut x_: T = x;
+
+    // Ensure that the angle is within the accurate range of the tailor series. 
+    if x_ < -pi_half
+    {   
+        let delta:T = x+pi_half;
+        x_ = -pi_half+delta.abs();
+    }
+    else if pi_half < x
+    {
+        let delta:T = x-pi_half;
+        x_ = pi_half-delta.abs();
+    }
+
+    // Calculate sine by using 
+    let mut sinx = x_-( powi(x_,3)/<T>::from_num(6) );
+    sinx += powi(x_,5)/<T>::from_num(120);
+    sinx -= (powi(x_,7)/<T>::from_num(2520)) >> 1;
+    sinx += (powi(x_,9)/<T>::from_num(2835)) >> 7;
+    return sinx;
 }
 
 /// Calculate atan(y/x) using a polynomial approximation.
@@ -28,12 +88,12 @@ pub fn powi<T>( base:T, power:usize ) -> T
 /// 
 /// \[1\] R. G. Lyons, Streamlining Digital Signal Processing, Second Etition, IEEE Press, 2012.
 /// 
-/// # Arguments 
+/// ## Arguments 
 ///
 /// * `y` - Is the argument along the y or imaginary axis.
 /// * `x` - Is the argument along the x or real axis.
 /// 
-/// # Example
+/// ## Example
 /// 
 /// ```
 /// use fixed_trigonometry::*;
@@ -58,12 +118,12 @@ pub fn atan2<T>( y: T, x: T ) -> T
 /// 
 /// \[1\] R. G. Lyons, Streamlining Digital Signal Processing, Second Etition, IEEE Press, 2012.
 /// 
-/// # Arguments 
+/// ## Arguments 
 ///
 /// * `y` - Is the argument along the y or imaginary axis.
 /// * `x` - Is the argument along the x or real axis.
 /// 
-/// # Example
+/// ## Example
 /// 
 /// ```
 /// use fixed_trigonometry::*;
@@ -78,7 +138,6 @@ pub fn atan<T>( x: T ) -> T
     return ( (x) + T::from_num(0.372003f32)*powi(x,3) ) 
             / (T::from_num(1) + T::from_num(0.703384f32)*powi(x,2) + T::from_num(0.043562f32)*powi(x,4) );
 }
-
 
 
 #[cfg(test)]
